39e4cb058b170339c88e829c0755a10f42510114
ASUS <ASUS@DESKTOP-MUGET55>
2024-07-01 21:31:46 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Прямоугольник
+    {
+        private double width; // Ширина прямоугольника
+        private double height; // Высота прямоугольника
+
+        public Прямоугольник(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        // Метод для вычисления площади прямоугольника
+        public double Площадь()
+        {
+            return width * height;
+        }
+        public double Периметр()
+        {
+            return (width + height) * 2;
+        }
+    }
+
+
+}
